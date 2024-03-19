cc0dc88942f8c0ea874170a9d241129202a0d6b0
Ryan Maffit <ryan@maffit.net>
2024-03-19 00:03:37 -0400

    Configured webDir

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'net.maffit.capacitoractions',
   appName: 'Capacitor Actions',
-  webDir: 'dist',
+  webDir: 'dist/capacitor-actions/browser',
   server: {
     androidScheme: 'https'
   }
